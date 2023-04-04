98116839a6018dd34db17da95165a5732bd6fa7a
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-04 18:11:58 +0300

    error

--- BackEnd/MiddleWare/route-not-found.ts
+++ BackEnd/MiddleWare/route-not-found.ts
@@ -1,2 +1,9 @@
+import { RouteNotFoundError } from '../Models/Client-Errors';
 import { Request,Response,NextFunction } from "express";
 
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const ErrorHandler = (request:Request, response:Response, next:NextFunction) =>{
+const err = new RouteNotFoundError(request.originalUrl)
+next(err)
+}
--- BackEnd/Models/Client-Errors.ts
+++ BackEnd/Models/Client-Errors.ts
@@ -14,3 +14,15 @@ export class RouteNotFoundError extends ClientError{
         super(404,`route ${route} not found`)
     }
 }
+
+export class VideoNotFoundError extends ClientError{
+    public constructor(videoID:string){
+        super(404,`video id: ${videoID} not found`)
+    }
+}
+
+export class UserNotLoggedError extends ClientError{
+    public constructor(){
+        super(401,`PLS login...`)
+    }
+}
