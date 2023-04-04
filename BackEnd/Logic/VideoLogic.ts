4c564f82aaa62e26d26665751e0941f0ae6cb430
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-04 17:43:22 +0300

    s

--- BackEnd/Logic/VideoLogic.ts
+++ BackEnd/Logic/VideoLogic.ts
@@ -1,10 +1,12 @@
-import Song from '../../src/model/Song'
+// import Song from '../../src/model/Song'
 
-const videoList=()=>{
-let song = new Song('asd1','asd2','asd3','asd4','asd5');
-return song;
-}
+// const videoList=():Promise<Song[]>=>{
+// let songsList:Song[] = []
+// let song = new Song('asd1','asd2','asd3','asd4','asd5');
+// songsList.push(song);
+// return songsList
+// }
 
-const videoByName=()=>{}
+// const videoByName=()=>{}
 
-export default { videoList,videoByName }
+// export default { videoList,videoByName }
