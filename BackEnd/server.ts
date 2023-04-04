2881a0bea122bbf09e6fc89f4902c06e2869476d
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-04 17:14:01 +0300

    delete

--- BackEnd/server.ts
+++ BackEnd/server.ts
@@ -12,6 +12,14 @@ const router = express.Router()
 router.post('/addVideo',async(request:Request,response:Response,next:NextFunction)=>{
     const body = request.body
     console.log(body)
+    response.status(201).json("{'msg':'video uploaded'}")
 })
 
-
+router.delete('/deleVideo/:id',async(request:Request,response:Response,next:NextFunction)=>{
+    const videoId = +request.params.id
+    if(videoId===null || videoId<1){
+        response.status(404).json("{'msg':'Video not found'}")
+    }
+    console.log("deleting....")
+    response.status(204)
+})
