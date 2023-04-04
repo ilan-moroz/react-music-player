12ac783cbc1924a7f00f2a3d2d314ae37266bdca
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-04 17:33:10 +0300

    routse

---
+++ BackEnd/Logic/VideoLogic.ts
@@ -0,0 +1,10 @@
+import Song from '../../src/model/Song'
+
+const videoList=()=>{
+let song = new Song('asd1','asd2','asd3','asd4','asd5');
+return song;
+}
+
+const videoByName=()=>{}
+
+export default { videoList,videoByName }
---
+++ BackEnd/Routes/VideoRoutes.ts
@@ -0,0 +1,31 @@
+import cors from 'cors'
+import express,{NextFunction,Request,Response} from 'express'
+import fileUpload from 'express-fileUpload'
+import config from '../Utils/Config'
+import { request } from 'http'
+
+
+
+
+const router = express.Router()
+
+router.post('/addVideo',async(request:Request,response:Response,next:NextFunction)=>{
+    const body = request.body
+    console.log(body)
+    response.status(201).json("{'msg':'video uploaded'}")
+})
+
+router.delete('/deleVideo/:id',async(request:Request,response:Response,next:NextFunction)=>{
+    const videoId = +request.params.id
+    if(videoId===null || videoId<1){
+        response.status(404).json("{'msg':'Video not found'}")
+    }
+    console.log("deleting....")
+    response.status(204)
+})
+
+router.get(
+    '/videoList',async(request:Request,response:Response,next:NextFunction)=>{
+        response.status(200).json()
+    } 
+)
