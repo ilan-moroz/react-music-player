96dc028e580f6b8f4db9a8f98c754ea170a02fbb
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-05 12:50:27 +0300

    dal

--- BackEnd/Utils/dal_mysql.ts
+++ BackEnd/Utils/dal_mysql.ts
@@ -3,8 +3,24 @@ import config from './Config'
 
 // create connection object
 const connection = mysql.createPool({
-    host: config.mySQLhost,
-    user: config.mySQLuser,
-    password: config.mySQLpass,
-    database: config.mySQLdatabase
+  host: config.mySQLhost,
+  user: config.mySQLuser,
+  password: config.mySQLpass,
+  database: config.mySQLdatabase,
 })
+
+console.log('we are connected to MySQL')
+
+const execute = (sql: string): Promise<any> => {
+  return new Promise<any>((resolve, reject) => {
+    connection.query(sql, (err, result) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(result)
+    })
+  })
+}
+
+export default execute
