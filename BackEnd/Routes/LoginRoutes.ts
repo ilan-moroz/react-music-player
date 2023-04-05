6b8fb6092973fc2778a301b8b391a75bdabd2d52
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-05 12:04:21 +0300

    route

--- BackEnd/Routes/LoginRoutes.ts
+++ BackEnd/Routes/LoginRoutes.ts
@@ -12,4 +12,14 @@ loginRouter.post('/register',async(require:Request,response:Response,next:NextFu
     response.status(201).json("{'msg':'welcome new user'}")
 });
 
+loginRouter.delete('/deleteUser/;userId',async(require:Request,response:Response,next:NextFunction) => {
+    console.log('delete user')
+    response.status(201).json("{'msg':'user deleted'}")
+});
+
+loginRouter.put('/updateUser/;userId',async(require:Request,response:Response,next:NextFunction) => {
+    console.log('update user')
+    response.status(202).json("{'msg':'user updated'}")
+});
+
 export default loginRouter
